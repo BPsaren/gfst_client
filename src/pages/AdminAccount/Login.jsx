e46cb16f10796949f034ada68d661b5c163e8929
Coding_herel <94925561+BPsaren@users.noreply.github.com>
2024-08-31 01:23:02 +0530

    Update Login.jsx

--- src/pages/AdminAccount/Login.jsx
+++ src/pages/AdminAccount/Login.jsx
@@ -27,7 +27,7 @@ export const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch(`http://localhost:3000/api/auth/login`, {
+      const response = await fetch(`https://gfst-client.vercel.app/api/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
