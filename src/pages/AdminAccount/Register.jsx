06b9d2c6719bc581570624f901cf31ba96721f0a
Coding_herel <94925561+BPsaren@users.noreply.github.com>
2024-08-31 01:30:15 +0530

    Update Register.jsx

--- src/pages/AdminAccount/Register.jsx
+++ src/pages/AdminAccount/Register.jsx
@@ -29,7 +29,7 @@ export const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch(`http://localhost:3000/api/auth/register`, {
+      const response = await fetch(`https://gfst-server.vercel.app/api/auth/register`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
